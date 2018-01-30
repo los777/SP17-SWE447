cf1d166d7e411f96605a20ae80f4c9ec4f5d1e83
los777 <35516716+los777@users.noreply.github.com>
2018-01-29 17:09:01 -0800

    Update cone.js

--- Common/Assignment-3/cone.js
+++ Common/Assignment-3/cone.js
@@ -3,10 +3,11 @@ var cone = null;
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
+  
+    gl = WebGLUtils.setupWebGL( canvas );
     
     cone = new Cone(gl,n);
 
-    gl = WebGLUtils.setupWebGL( canvas );
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
