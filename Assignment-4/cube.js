5c33296863daa2c8528e87999894d789a7fa8c8b
los777 <35516716+los777@users.noreply.github.com>
2018-02-06 21:06:14 -0800

    Update cube.js

--- Assignment-4/cube.js
+++ Assignment-4/cube.js
@@ -7,14 +7,16 @@ function init() {
   
     gl = WebGLUtils.setupWebGL( canvas );
    
-    square = new Square( gl);
+   
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
     }
 
-    gl.clearColor( 1.0, 1.0, 0.0, 1.0 );
+    gl.clearColor( 0.0, 1.0, 1.0, 1.0 );
+    
+     square = new Square( gl);
 
     
     
