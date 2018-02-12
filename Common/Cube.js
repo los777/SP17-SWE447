4ee356db24335b4c9d9e47a427a5acf0e3425970
los777 <35516716+los777@users.noreply.github.com>
2018-02-11 19:23:31 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -19,6 +19,42 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.positions = { 
         values : new Float32Array([
            // Add your list vertex positions here
+          // Front face
+           0.0,  0.0,  1.0,
+           1.0,  0.0,  1.0,
+           1.0,  1.0,  1.0,
+           0.0,  1.0,  1.0,
+
+          // Back face
+           0.0,  0.0,  0.0,
+           0.0,  1.0,  0.0,
+           1.0,  1.0,   0.0,
+           1.0,  0.0,   0.0,
+
+          // Top face
+           0.0,  1.0,  0.0,
+           0.0,  1.0,  1.0,
+           1.0,  1.0,  1.0,
+           1.0,  1.0,  0.0,
+
+          // Bottom face
+           0.0,  0.0,  0.0,
+           1.0,  0.0,  0.0,
+           1.0,  0.0,  1.0,
+           0.0,  0.0,  1.0,
+
+          // Right face
+           1.0,  0.0,  0.0,
+           1.0,  1.0,  0.0,
+           1.0,  1.0,  1.0,
+           1.0,  0.0,  1.0,
+
+          // Left face
+           0.0,  0.0,  0.0,
+           0.0,  0.0,  1.0,
+           0.0,  1.0,  1.0,
+           0.0,  1.0,  0.0,
+
             ]),
         numComponents : 3
     };
@@ -26,6 +62,13 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.indices = { 
         values : new Uint16Array([
             // Add your list of triangle indices here
+                0,  1,  2,      0,  2,  3,    // front
+                4,  5,  6,      4,  6,  7,    // back
+                8,  9,  10,     8,  10, 11,   // top
+                12, 13, 14,     12, 14, 15,   // bottom
+                16, 17, 18,     16, 18, 19,   // right
+                20, 21, 22,     20, 22, 23,   // left
+
         ])
     };
     this.indices.count = this.indices.values.length;
