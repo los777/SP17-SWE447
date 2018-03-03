3d8b72e965c3796c7387fb559c0f036435f81837
los777 <35516716+los777@users.noreply.github.com>
2018-03-02 20:46:03 -0800

    Update MatrixStack.js

--- Common/MatrixStack.js
+++ Common/MatrixStack.js
@@ -1,20 +1,22 @@
-//
 //  MatrixStack.js
-//
+//  requires MV.js
 
-function MatrixStack() {
+function MatrixStack()
+{
   this.stack = [ mat4() ];
   this.current = function ()  { return this.stack[0]; };
   this.push = function ()  { this.stack.unshift( this.stack[0] ); };
   this.pop = function ()  { this.stack.shift(); };
   this.load = function (m) { this.stack[0] = m; };
+  // Remember that the order matters here; we need to post-mult
   this.mult = function (m) { this.stack[0] = mult( this.stack[0], m ); };
   this.loadIdentity = function ()  { this.stack[0] = mat4(); };
 
-  this.rotate = function (angle, axis) { 
+  this.rotate = function (angle, axis) {
     this.stack[0] = mult( this.stack[0], rotate(angle, axis) );
   };
   this.scale = function(x, y, z) {
+    // Just check to see if the variables have been assigned a value
     if (y === undefined && z === undefined) {
       y = x;
       z = x;
@@ -22,6 +24,6 @@ function MatrixStack() {
     this.stack[0] = mult( this.stack[0], scalem(x, y, z) );
   };
   this.translate = function(x, y, z) {
-   this.stack[0] = mult( this.stack[0], translate(x, y, z) ); 
+   this.stack[0] = mult( this.stack[0], translate(x, y, z) );
   };
 };
